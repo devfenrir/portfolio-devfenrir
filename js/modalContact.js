bfa4fc91ad7ce6a6f4f6e73f5781b4376f7f33dc
Liperdev <felipe.devsoftwareengineer@gmail.com>
2025-06-20 19:03:39 -0300

    acrescentado modal

--- js/modalContact.js
+++ js/modalContact.js
@@ -1,8 +1,14 @@
-// Em desenvolvimento
 const sendEmailButton = document.querySelector('[data-button-send-email]')
+const sectionOverlay = document.querySelector('[data-section-overlay]')
+const sectionClosedButton = document.querySelector('[data-section-closed]')
 
 const openModalEmail = () => {
-    
+    sectionOverlay.classList.remove('invisible')
 }
 
-sendEmailButton.addEventListener('click', )
+const closeModalEmail = () => {
+    sectionOverlay.classList.add('invisible')
+}
+
+sendEmailButton.addEventListener('click', openModalEmail)
+sectionClosedButton.addEventListener('click', closeModalEmail)
