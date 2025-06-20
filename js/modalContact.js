062782fed15ad26fac572f725d8167ae61a9e9dc
Liperdev <felipe.devsoftwareengineer@gmail.com>
2025-06-20 15:53:09 -0300

    contact ajuste

---
+++ js/modalContact.js
@@ -0,0 +1,8 @@
+// Em desenvolvimento
+const sendEmailButton = document.querySelector('[data-button-send-email]')
+
+const openModalEmail = () => {
+    
+}
+
+sendEmailButton.addEventListener('click', )
