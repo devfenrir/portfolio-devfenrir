7f78fdf31563add8a60d01d2d40c274f963b5a1e
Liperdev <felipe.devsoftwareengineer@gmail.com>
2025-06-21 02:24:49 -0300

    comentários modal

--- js/modalContact.js
+++ js/modalContact.js
@@ -2,13 +2,16 @@ const sendEmailButton = document.querySelector('[data-button-send-email]')
 const sectionOverlay = document.querySelector('[data-section-overlay]')
 const sectionClosedButton = document.querySelector('[data-section-closed]')
 
+// Função abre modal
 const openModalEmail = () => {
     sectionOverlay.classList.remove('invisible')
 }
 
+// Função fecha modal
 const closeModalEmail = () => {
     sectionOverlay.classList.add('invisible')
 }
 
+// Escutadores de evento que recebem funções 
 sendEmailButton.addEventListener('click', openModalEmail)
 sectionClosedButton.addEventListener('click', closeModalEmail)
