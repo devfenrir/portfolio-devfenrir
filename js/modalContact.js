bc8281ca69c2089a94ffe16aad86df5f2cd9ca6e
Liperdev <felipe.devsoftwareengineer@gmail.com>
2025-06-21 17:23:01 -0300

    acrescentado animacao modal

--- js/modalContact.js
+++ js/modalContact.js
@@ -1,17 +1,20 @@
 const sendEmailButton = document.querySelector('[data-button-send-email]')
 const sectionOverlay = document.querySelector('[data-section-overlay]')
 const sectionClosedButton = document.querySelector('[data-section-closed]')
+const janelaModal = document.querySelector('[data-janela-modal]')
 
 // Função abre modal
 const openModalEmail = (evento) => {
     evento.preventDefault()
     sectionOverlay.classList.remove('invisible')
+    janelaModal.classList.add('animacao-fadeIn')
 }
 
 // Função fecha modal
 const closeModalEmail = (evento) => {
     evento.preventDefault()
     sectionOverlay.classList.add('invisible')
+    janelaModal.classList.remove('animacao-fadeIn')
 }
 
 // Escutadores de evento que recebem funções 
