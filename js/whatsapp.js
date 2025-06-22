1776eefefcf38c060250d036a45ee2af8c008351
Liperdev <felipe.devsoftwareengineer@gmail.com>
2025-06-22 19:20:53 -0300

    whatsapp component

---
+++ js/whatsapp.js
@@ -0,0 +1,22 @@
+const whatsappComponent = document.querySelector('[data-section-whatsapp]');
+
+const scrollWhatsapp = (component, scrollPosition) => {
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > scrollPosition) {
+            component.classList.remove('animacao-left-returned', 'invisible');
+            component.classList.add('animacao-left');
+        } else {
+            if (!component.classList.contains('invisible')) {
+                component.classList.remove('animacao-left');
+                component.classList.add('animacao-left-returned');
+
+                component.addEventListener('animationend', function hideAfterAnimation() {
+                    component.classList.add('invisible');
+                    component.removeEventListener('animationend', hideAfterAnimation);
+                });
+            }
+        }
+    });
+};
+
+scrollWhatsapp(whatsappComponent, 200);
