fc2be964fe8c0a4e0a4db63376cbb70c35064a11
Liperdev <felipe.devsoftwareengineer@gmail.com>
2025-06-26 21:07:40 -0300

    conclusão portfólio dev

--- js/scroll.js
+++ js/scroll.js
@@ -1,60 +1,86 @@
-// Variaveis Gerais para interação com scroll 
+document.addEventListener('DOMContentLoaded', () => {
 
-const headerComponent = document.querySelector('[data-header]')
-const sectionAboutMe = document.querySelector('[data-section-about-me]')
-const sectionKills = document.querySelector('[data-section-skills]')
-const sectionSkillsItems = document.querySelector('[data-section-skills-items]')
-const sectionProjects = document.querySelector('[data-section-projects]')
-const sectionContactComponent = document.querySelector('[data-section-contact]')
+    // Variaveis Gerais para interação com scroll 
+
+    const headerComponent = document.querySelector('[data-header]')
+    const sectionAboutMe = document.querySelector('[data-section-about-me]')
+    const sectionDescricaoAbout = document.querySelector('[data-section-descricao-about-me]')
+    const sectionKills = document.querySelector('[data-section-skills]')
+    const sectionSkillsItems = document.querySelector('[data-section-skills-items]')
+    const sectionProjects = document.querySelector('[data-section-projects]')
+    const sectionContactComponent = document.querySelector('[data-section-contact]')
+
+    const sectionProjectOne = document.querySelector('[data-project-one]')
+    const sectionProjectTwo = document.querySelector('[data-project-two]')
+    const sectionProjectThree = document.querySelector('[data-project-three]')
 
 
-// Funcao responsável por detectar o elemento e a posicaoAtualScroll
+    // Funcao responsável por detectar o elemento e a posicaoAtualScroll
 
-function animateOnScroll(element, scrollPositionTrigger) {
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > scrollPositionTrigger) {
-            element.classList.add('opacity-100', 'translate-y-0', 'transition-all', 'duration-500')
-            element.classList.remove('opacity-0', 'translate-y-5')
-        } else {
-            element.classList.remove('opacity-100', 'translate-y-0')
-            element.classList.add('opacity-0', 'translate-y-5', 'transition-all', 'duration-500')
-        }
-    })
-}
+    function animateOnScroll(element, scrollPositionTrigger) {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > scrollPositionTrigger) {
+                element.classList.add('opacity-100', 'translate-y-0', 'transition-all', 'duration-700', 'ease-in-out')
+                element.classList.remove('opacity-0', 'translate-y-5')
+            } else {
+                element.classList.remove('opacity-100', 'translate-y-0', 'transition-all', 'duration-700', 'ease-in-out')
+                element.classList.add('opacity-0', 'translate-y-5', 'transition-all', 'duration-500')
+            }
+        })
+    }
 
-// Função responsável para causar o efeito no header
+    // Função responsável para causar o efeito no header
 
-function animateHeader(scrollPositionTrigger) {
-    window.addEventListener('scroll', () => {
-        if (window.scrollY > scrollPositionTrigger) {
-            headerComponent.classList.add(
-                'backdrop-blur-md',
-                'bg-black/30',
-                'border-b',
-                'border-white/10',
-                'shadow-md',
-                'transition-all',
-                'duration-300'
-            )
-        } else {
-            headerComponent.classList.remove(
-                'backdrop-blur-md',
-                'bg-black/30',
-                'border-b',
-                'border-white/10',
-                'shadow-md',
-                'transition-all',
-                'duration-300'
-            )
-        }
-    })
-}
+    function animateHeader(scrollPositionTrigger) {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > scrollPositionTrigger) {
+                headerComponent.classList.add(
+                    'backdrop-blur-md',
+                    'bg-black/30',
+                    'border-b',
+                    'border-white/10',
+                    'shadow-md',
+                    'transition-all',
+                    'duration-300'
+                )
+            } else {
+                headerComponent.classList.remove(
+                    'backdrop-blur-md',
+                    'bg-black/30',
+                    'border-b',
+                    'border-white/10',
+                    'shadow-md',
+                    'transition-all',
+                    'duration-300'
+                )
+            }
+        })
+    }
 
-// Invocações das funções
+    // Função responsável para causar o efeito dos itens das skills
 
-animateHeader(30)
-animateOnScroll(sectionAboutMe, 290)
-animateOnScroll(sectionKills, 1150)
-animateOnScroll(sectionSkillsItems, 1300)
-animateOnScroll(sectionProjects, 1850)
-animateOnScroll(sectionContactComponent, 3000)
+    function animacaoLado(element, scrollPositionTrigger) {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > scrollPositionTrigger) {
+                element.classList.add('opacity-100', 'translate-y-0', 'transition-all', 'duration-700', 'ease-in-out', 'animacaoCards')
+                element.classList.remove('opacity-0', 'translate-y-5')
+            } else {
+                element.classList.remove('opacity-100', 'translate-y-0', 'transition-all', 'duration-700', 'ease-in-out', 'animacaoCards')
+                element.classList.add('opacity-0', 'translate-y-5', 'transition-all', 'duration-500')
+            }
+        })
+    }
+
+    // Invocações das funções
+
+    animateHeader(30)
+    animateOnScroll(sectionAboutMe, 290)
+    animacaoLado(sectionDescricaoAbout, 500)
+    animateOnScroll(sectionKills, 1150)
+    animacaoLado(sectionSkillsItems, 1460)
+    animateOnScroll(sectionProjects, 2000)
+    animacaoLado(sectionProjectOne, 2050)
+    animacaoLado(sectionProjectTwo, 2070)
+    animacaoLado(sectionProjectThree, 1690)
+    animateOnScroll(sectionContactComponent, 3000)
+})
