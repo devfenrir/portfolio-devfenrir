2b75b45da8ba559f91c3851edb5a87052007d372
Liperdev <felipe.devsoftwareengineer@gmail.com>
2025-07-13 20:58:40 -0300

    adição de espaçamento entre essa mensagem

--- js/certificationsHidden.js
+++ js/certificationsHidden.js
@@ -3,7 +3,6 @@ const botaoVerMaisCertificados = document.querySelector('[data-button-mais-detal
 botaoVerMaisCertificados.addEventListener('click', () => {
     const divMessagePersonalizada = document.querySelector('[data-dont-have-message]')
 
-
     if (divMessagePersonalizada.children.length === 0) {
         const dontHaveMessage = document.createElement('p')
         dontHaveMessage.innerText = 'Não há mais certificados até o momento'
@@ -12,7 +11,8 @@ botaoVerMaisCertificados.addEventListener('click', () => {
             'text-slate-500',
             'font-bold',
             'text-center',
-            'text-[20px]'
+            'text-[20px]',
+            'pt-4'
         )
 
         divMessagePersonalizada.appendChild(dontHaveMessage)
