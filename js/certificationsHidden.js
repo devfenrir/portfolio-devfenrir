e142e78c4c3a2c8a18e5e2f872859f22e6145536
Liperdev <felipe.devsoftwareengineer@gmail.com>
2025-07-13 20:56:53 -0300

    alternativa para mensagem enquanto não há outros certificados registrados

--- js/certificationsHidden.js
+++ js/certificationsHidden.js
@@ -1,1 +1,20 @@
-// Em desenvolvimento
+const botaoVerMaisCertificados = document.querySelector('[data-button-mais-detalhes-certificados]')
+
+botaoVerMaisCertificados.addEventListener('click', () => {
+    const divMessagePersonalizada = document.querySelector('[data-dont-have-message]')
+
+
+    if (divMessagePersonalizada.children.length === 0) {
+        const dontHaveMessage = document.createElement('p')
+        dontHaveMessage.innerText = 'Não há mais certificados até o momento'
+
+        dontHaveMessage.classList.add(
+            'text-slate-500',
+            'font-bold',
+            'text-center',
+            'text-[20px]'
+        )
+
+        divMessagePersonalizada.appendChild(dontHaveMessage)
+    }
+})
