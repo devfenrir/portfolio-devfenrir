2b39c2466b5f5ea6f705a49515a44e753c9a5635
Liperdev <felipe.devsoftwareengineer@gmail.com>
2025-07-19 19:28:55 -0300

    adiciona tradução

---
+++ js/tradutions.js
@@ -0,0 +1,26 @@
+const translations = {
+  pt: {
+    "nav-home": "início",
+    "nav-sobre": "sobre",
+    "nav-habilidades": "habilidades",
+    "nav-projetos": "projetos",
+    "nav-certificacoes": "certificações",
+    "nav-contato": "contato"
+  },
+  en: {
+    "nav-home": "home",
+    "nav-sobre": "about",
+    "nav-habilidades": "skills",
+    "nav-projetos": "projects",
+    "nav-certificacoes": "certifications",
+    "nav-contato": "contact"
+  },
+  es: {
+    "nav-home": "inicio",
+    "nav-sobre": "sobre",
+    "nav-habilidades": "habilidades",
+    "nav-projetos": "proyectos",
+    "nav-certificacoes": "certificaciones",
+    "nav-contato": "contacto"
+  }
+};
